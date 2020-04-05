94b481400fa72a38578f60b2b11274897d5e7d93
JurekMateusz <56219248+JurekMateusz@users.noreply.github.com>
2020-04-05 17:08:17 +0200

    refactore skarbfinder

--- src/main/java/spoj/skarbfinder/SkarbFinder.java
+++ src/main/java/spoj/skarbfinder/SkarbFinder.java
@@ -1,6 +1,6 @@
 //https://pl.spoj.com/problems/SKARBFI/
 
-package  spoj.skarbfinder;
+package spoj.skarbfinder;
 
 import java.awt.*;
 import java.util.LinkedList;
@@ -9,96 +9,101 @@ import java.util.Scanner;
 public class SkarbFinder {
     private static Point point;
     private static Point oldPoint;
-    private static LinkedList<String> resoult;
-    private static long NORTH = 0;
-    private static long SOUT = 0;
-    private static long EAST = 0;
-    private static long WEST = 0;
+    private static LinkedList<String> result;
+    Scanner myReader;
+    private int[] directions; //NORTH ,SOUT,EAST,WEST
 
-    public static void fun(){
-        resoult = new LinkedList<>();
+    public SkarbFinder() {
+        result = new LinkedList<>();
         point = new Point();
         oldPoint = new Point(0, 0);
+        myReader = new Scanner(System.in);
+    }
 
-        Scanner myReader = new Scanner(System.in);
 
+    public void fun() {
         int numbersOfSeries = myReader.nextInt();
+
         for (int i = 0; i < numbersOfSeries; i++) {
-            int numbersOfPairs = myReader.nextInt();
-            myReader.nextLine();
-            for (int j = 0; j < numbersOfPairs; j++) {
-                String line = myReader.nextLine();
-                String[] splitLine = line.split(" ");
-                int dat1 = Integer.parseInt(splitLine[0]);
-                long dat2 = Integer.parseInt(splitLine[1]);
-                switch (dat1) {
-                    case 0:
-                        //NORTH
-                        NORTH += dat2;
-                        break;
-                    case 1:
-                        //SOUT
-                        SOUT -= dat2;
-                        break;
-                    case 2:
-                        //WEST
-                        WEST -= dat2;
-                        break;
-                    case 3:
-                        //EAST
-                        EAST += dat2;
-                        break;
-                    default:
-                        System.err.println("not  allowed number");
-                }
-            }
+            directions = new int[]{0, 0, 0, 0};
+            readSerie();
+
             oldPoint.setLocation(point.getX(), point.getY());
-            point.setLocation(point.getX() +(EAST + WEST), point.getY()+(NORTH + SOUT));
+            double newX = point.getX() + (directions[2] + directions[3]);
+            double newY = point.getY() + (directions[0] + directions[1]);
+            point.setLocation(newX, newY);
 
             if (oldPoint.equals(point)) {
-                resoult.add("studnia");
+                result.add("studnia");
                 continue;
             }
-            if (point.getY() != oldPoint.getY()) {
-                int count = 0;
-                if (point.getY() > oldPoint.getY()) {
-                    while (count != point.getY()) {
-                        count++;
-                    }
-                    resoult.add("0 " + (count - (int) oldPoint.getY()));
-                } else {
-                    while (count != point.getY()) {
-                        count--;
-                    }
-                    resoult.add("1 " + Math.abs(count + (int) oldPoint.getY()));
-                }
-            }
-            if (point.getX() != oldPoint.getX()) {
-                int count = 0;
-                if (point.getX() > oldPoint.getX()) {
-                    while (count != point.getX()) {
-                        count++;
-                    }
-                    resoult.add("3 " + (count - (int) oldPoint.getX()));
-                } else {
-                    while (count != point.getX()) {
-                        count--;
-                    }
-                    resoult.add("2 " + Math.abs(count + (int) oldPoint.getX()));
-                }
-            }
-            WEST = 0;
-            NORTH = 0;
-            SOUT = 0;
-            EAST = 0;
+            countSteps();
         }
         myReader.close();
-        for (String n : resoult) {
+
+        for (String n : result) {
             System.out.println(n);
         }
     }
-    public static void main(String[] args) throws java.lang.Exception{
-        fun();
+
+    private void readSerie() {
+        int numbersOfPairs = myReader.nextInt();
+        for (int j = 0; j < numbersOfPairs; j++) {
+            int direction = myReader.nextInt();
+            int numberOfSteps = myReader.nextInt();
+            addDir(direction, numberOfSteps);
+        }
     }
 
+    private void addDir(int direction, int numberOfSteps) {
+        switch (direction) {
+            case 0:
+                directions[0] += numberOfSteps;
+                break;
+            case 1:
+                directions[1] -= numberOfSteps;
+                break;
+            case 2:
+                directions[3] -= numberOfSteps;
+                break;
+            case 3:
+                directions[2] += numberOfSteps;
+                break;
+            default:
+                System.err.println("not  allowed number");
+        }
+
+    }
+
+    private void countSteps() {
+        if (point.getY() != oldPoint.getY()) {
+            countYYSteps();
+        }
+        if (point.getX() != oldPoint.getX()) {
+            countXXSteps();
+        }
+    }
+
+    private void countYYSteps() {
+        int count = (int) (point.getY() - oldPoint.getY());
+        if (point.getY() > oldPoint.getY()) {
+            result.add("0 " + count);
+        } else {
+            result.add("1 " + Math.abs(count));
+        }
+    }
+
+    private void countXXSteps() {
+        int count = (int) (point.getX() - oldPoint.getX());
+        if (point.getX() > oldPoint.getX()) {
+            result.add("3 " + count);
+        } else {
+            result.add("2 " + Math.abs(count));
+        }
+    }
+
+    public static void main(String[] args) throws java.lang.Exception {
+        new SkarbFinder().fun();
+    }
 }
+
